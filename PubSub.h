90342bdffda27e14a6b0bc15890e7fec429608c4
komat <komatsu@birdman.ne.jp>
2016-08-23 00:57:45 +0900

    modify

--- PubSub.h
+++ PubSub.h
@@ -15,42 +15,126 @@
 #include <map>
 
 
-typedef void (*topicFunctionPtr)(std::string topic, void * topicOption);
-
-
+template<
+        class TopicHandler = std::function<void()>
+>
 class PubSub {
-protected:
-    void *_context;
-
 private:
 
-    typedef struct topic_data {
-        topicFunctionPtr subscriber;
-    } TopicData;
-
-
-    std::map<const std::string, std::map<int, std::vector<TopicData> > > _subscriberList;
-    std::map<const std::string,  std::vector<TopicData> > _subscriberOnceList;
-
+    std::map<const std::string, std::map<int, std::vector<TopicHandler> > > _subscriberList;
+    std::map<const std::string, std::vector<TopicHandler> > _subscriberOnceList;
 
 public:
-    PubSub();
 
-    virtual ~PubSub();
+    template<class ...Args>
+    void publish(const std::string &topic, Args &&... args) {
 
-    void publish(std::string topic, void *topicOption);
+        if (!hasTopic(topic)) {
+            return;
+        }
 
-    void subscribe(const std::string &topic, topicFunctionPtr subscriber, int priority = 0);
+        auto &topicList = _subscriberList[topic];
 
-    void subscribeOnce(const std::string &topic, topicFunctionPtr subscriber, int priority = 0);
+        for (auto priorityList = topicList.rbegin(); priorityList != topicList.rend(); ++priorityList) {
+            auto &subscriberList = priorityList->second;
 
-    void unsubscribe(const std::string &topic);
+            for (auto priorityItem = subscriberList.rbegin(); priorityItem != subscriberList.rend();) {
+                auto &listener = *priorityItem;
+                listener(topic, std::forward<Args>(args)...);
 
-    void unsubscribe(const std::string &topic, topicFunctionPtr subscriber);
+                if (_subscriberOnceList.count(topic)) {
+                    if (!_subscriberOnceList[topic].empty()) {
+                        if (_subscriberOnceList[topic].count(&listener)) {
+                            priorityItem = subscriberList.erase(priorityItem);
+                            _subscriberOnceList[topic].erase(&listener);
+                        } else {
+                            ++priorityItem;
+                        }
 
-    void unsubscribeAll(const std::string &topic);
+                    } else {
+                        ++priorityItem;
+                    }
+                } else {
+                    ++priorityItem;
+                }
 
-    bool hasTopic(const std::string &topic);
+//                if (_subscriberOnceList.count(topic)) {
+//                    if (!_subscriberOnceList[topic].empty()) {
+//                        auto onceItem = _subscriberOnceList[topic].begin();
+//                        while (onceItem != _subscriberOnceList[topic].end()) {
+//                            std::cout << (onceItem == listener ? "TRUE" : "FALSE") << std::endl;
+////                            if (onceItem == listener) {
+////                                priorityItem = priorityItem.eraes(onceItem);
+////                                onceItem = _subscriberOnceList[topic].erase(onceItem);
+////                            } else {
+//                                ++onceItem;
+////                            }
+//                        }
+//                    }
+//                }
+            }
+        }
+
+    };
+
+
+    void subscribe(const std::string &topic, TopicHandler subscriber, int priority = 0) {
+        _subscriberList[topic][priority].push_back(subscriber);
+    };
+
+
+    void subscribeOnce(const std::string &topic, TopicHandler subscriber, int priority = 0) {
+        _subscriberList[topic][priority].push_back(subscriber);
+        _subscriberOnceList[topic].push_back(subscriber);
+    };
+
+
+    void unsubscribe(const std::string &topic) {
+        unsubscribeAll(topic);
+    };
+
+
+    void unsubscribe(const std::string &topic, TopicHandler subscriber) {
+        std::map<int, std::vector<TopicHandler> > &topicList = _subscriberList[topic];
+
+//        for (std::map<int, std::vector<TopicHandler> >::reverse_iterator priorityList = topicList.rbegin();
+//             priorityList != topicList.rend(); ++priorityList) {
+//
+//            std::vector<TopicHandler> &subscriberList = priorityList->second;
+//            std::vector<TopicHandler>::iterator priorityItem;
+//
+//            for (priorityItem = subscriberList.begin(); priorityItem != subscriberList.end();) {
+//                if (priorityItem == subscriber) {
+//                    priorityItem = subscriberList.erase(
+//                            subscriberList.begin() + std::distance(subscriberList.begin(), priorityItem));
+//                    continue;
+//                }
+//                ++priorityItem;
+//            }
+//        }
+
+        if (topicList.empty()) _subscriberList.erase(topic);
+    };
+
+
+    void unsubscribeAll(const std::string &topic) {
+        if (!hasTopic(topic)) return;
+
+//        std::map<int, std::vector<TopicHandler> > &topicList = _subscriberList[topic];
+//        std::map<int, std::vector<TopicHandler> >::iterator priorityList = topicList.begin();
+//
+//        while (priorityList != topicList.end()) {
+//            topicList.erase(priorityList++);
+//        }
+
+//        if (topicList.empty()) _subscriberList.erase(topic);
+    };
+
+
+    bool hasTopic(const std::string &topic) {
+        return (_subscriberList.find(topic) != _subscriberList.end());
+    };
+
 
 };
 
--- main.cpp
+++ main.cpp
@@ -2,60 +2,61 @@
 #include "PubSub.h"
 
 
-PubSub pub_sub;
+//using TopicHandlerT = std::function< void( const std::string & str, const std::string & msg) >;
+using TopicHandlerT = std::function< void(const std::string &topic, const std::string & str) >;
 
-const std::string INITIALIZE = "initialize";
 
+PubSub<TopicHandlerT> pub_sub;
 
-void subscriber01(const std::string topic, void *data) {
-    std::cout << "(01) >>>>>>>>>>>>>" << topic << " : " << (char *) data <<  std::endl;
-    pub_sub.unsubscribe(INITIALIZE, (topicFunctionPtr) subscriber01);
+std::string INITIALIZE = "initialize";
+
+void subscriber01(const std::string &topic, const std::string & str) {
+    std::cout << "(01) >>>>>>>>>>>>>" << topic << "::" << str  <<  std::endl;
+//    pub_sub.unsubscribe("INITIALISE", subscriber01);
 }
 
 
-void subscriber02(const std::string topic, void *data) {
-    std::cout << "(02) /////////////" << topic << " : " << (char *) data << std::endl;
-    pub_sub.unsubscribe(INITIALIZE, (topicFunctionPtr) subscriber02);
+void subscriber02(const std::string &topic, const std::string & str) {
+    std::cout << "(02) /////////////" << topic << "::" << str  << std::endl;
+//    pub_sub.unsubscribe("INITIALISE", subscriber02);
 }
 
-void subscriber03(const std::string topic, void *data) {
-    std::cout << "(03) -------------" << topic << " : " << (char *) data << std::endl;
+void subscriber03(const std::string &topic, const std::string & str) {
+    std::cout << "(03) -------------" << topic << "::" << str  << std::endl;
 }
 
-void subscriber04(const std::string topic, void *data) {
-    std::cout << "(04) :::::::::::::" << topic << " : " << (char *) data << std::endl;
+void subscriber04(const std::string &topic, const std::string & str) {
+    std::cout << "(04) :::::::::::::" << topic << "::" << str  << std::endl;
 }
 
 
-void subscriber05(const std::string topic, void *data) {
-    std::cout << "(05) ~~~~~~~~~~~~" << topic << " : " << (char *) data << std::endl;
+void subscriber05(const std::string &topic, const std::string & str) {
+    std::cout << "(05) ~~~~~~~~~~~~" << topic << "::" << str  << std::endl;
 }
-
 
 int main() {
 
 
     std::cout << "\n[ READY ]\n" << std::endl;
 
-    pub_sub.subscribe(INITIALIZE, (topicFunctionPtr) subscriber01, 1);
-    pub_sub.subscribe(INITIALIZE, (topicFunctionPtr) subscriber02, 4);
-    pub_sub.subscribe(INITIALIZE, (topicFunctionPtr) subscriber03, 3);
-    pub_sub.subscribe(INITIALIZE, (topicFunctionPtr) subscriber04, 2);
-    pub_sub.subscribeOnce(INITIALIZE, (topicFunctionPtr) subscriber05, 2);
 
-    char charData[] = "PARAMS";
+    pub_sub.subscribe(INITIALIZE, subscriber01, 1);
+    pub_sub.subscribe(INITIALIZE, subscriber02, 4);
+    pub_sub.subscribe(INITIALIZE, subscriber03, 3);
+    pub_sub.subscribe(INITIALIZE, subscriber04, 2);
+    pub_sub.subscribeOnce(INITIALIZE, subscriber05, 2);
 
-    pub_sub.publish(INITIALIZE, &charData);
+    pub_sub.publish(INITIALIZE, "PARAMS");
 
-    std::cout << "\n[ PLAY01 ]\n" << std::endl;
-
-    pub_sub.publish(INITIALIZE, &charData);
-
-    pub_sub.unsubscribeAll(INITIALIZE);
-
-    std::cout << "\n[ PLAY02 ]\n" << std::endl;
-
-    pub_sub.publish(INITIALIZE, &charData);
+//    std::cout << "\n[ PLAY01 ]\n" << std::endl;
+//
+//    pub_sub.publish(INITIALIZE, &charData);
+//
+//    pub_sub.unsubscribeAll(INITIALIZE);
+//
+//    std::cout << "\n[ PLAY02 ]\n" << std::endl;
+//
+//    pub_sub.publish(INITIALIZE, &charData);
 
     return 0;
 }
