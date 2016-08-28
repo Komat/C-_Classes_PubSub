ad795271022d06cc3dcfdc7d70117c371e4bd5c5
komat <komatsu_takayuki@komat.local>
2016-08-28 12:45:19 +0900

    update

--- PubSub.h
+++ PubSub.h
@@ -16,18 +16,21 @@
 #include <vector>
 
 
-template<typename TopicHandler = std::function<void(const std::string &topic, const std::string & str)>>
+template<
+        typename Topic = std::string,
+        typename TopicHandler = std::function<void(const Topic &topic, const Topic &str)>
+>
 class PubSub {
 private:
 
-    std::size_t _topicCount = 0; // TODO: カウント数の定義・使用用途が曖昧なため現状使用不可
-    std::map<const std::string, std::map<int, std::vector<TopicHandler> > > _subscriberList;
-    std::map<const std::string, std::unordered_set<TopicHandler *> > _subscriberOnceList;
+    std::size_t _observerCount = 0; // TODO: カウント数の定義・使用用途が曖昧なため現状使用不可
+    std::map<const Topic, std::map<int, std::vector<TopicHandler> > > _subscriberList;
+    std::map<const Topic, std::unordered_set<TopicHandler *> > _subscriberOnceList;
 
 public:
 
     template<class ...Args>
-    void publish(const std::string &topic, Args &&... args) {
+    void publish(const Topic &topic, Args &&... args) {
 
         if (!hasTopic(topic)) {
             return;
@@ -59,21 +62,21 @@ public:
     };
 
 
-    void subscribe(const std::string &topic, TopicHandler subscriber, int priority) {
+    void subscribe(const Topic &topic, TopicHandler subscriber, int priority) {
         _subscriberList[topic][priority].push_back(subscriber);
-        ++_topicCount;
+        ++_observerCount;
     };
 
 
-    void subscribeOnce(const std::string &topic, TopicHandler subscriber, int priority = 0) {
+    void subscribeOnce(const Topic &topic, TopicHandler subscriber, int priority = 0) {
         auto &listeners = _subscriberList[topic][priority];
         listeners.push_back(subscriber);
         _subscriberOnceList[topic].insert(&listeners.back());
-        ++_topicCount;
+        ++_observerCount;
     };
 
 
-    void unsubscribe(const std::string &topic) {
+    void unsubscribe(const Topic &topic) {
 
         if (!hasTopic(topic)) {
             return;
@@ -103,13 +106,45 @@ public:
     void unsubscribeAll() {
         _subscriberList.clear();
         _subscriberOnceList.clear();
-        _topicCount = 0;
+        _observerCount = 0;
     };
 
 
-    bool hasTopic(const std::string &topic) {
+    bool hasTopic(const Topic &topic) {
         return (_subscriberList.find(topic) != _subscriberList.end());
     };
+
+
+
+    bool remove(const Topic &topic, TopicHandler &subscriber) {
+
+        if (!hasTopic(topic)) {
+            return false;
+        }
+
+        auto &topicList = _subscriberList[topic]; // std::map
+
+        for (auto &priorityList : topicList) {
+            auto &subscriberList = priorityList.second;
+
+            for (auto priorityItem = subscriberList.begin(); priorityItem != subscriberList.end();) {
+                if (&*priorityItem == &subscriber) {
+                    subscriberList.erase(priorityItem);
+                    --_observerCount;
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+
+    std::size_t countListeners() const {
+        return _observerCount;
+    }
+
 
 };
 
